96b4b50b203298a75b0aaafbe8e360591ca06a1c
greg111111 <99788750+greg111111@users.noreply.github.com>
2022-02-16 08:47:57 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 
 #BOT:
-TOKEN = "" # ТОКЕН БОТА
-ADMIN = 392192947 # ИД АДМИНА, узнать свой айди - @get_any_telegram_id_bot
+TOKEN = "5161492545:AAFxZeUX0qtbaNcMCUm_00xVGTqFOZIPFJ4" # ТОКЕН БОТА
+ADMIN = 50853306897 # ИД АДМИНА, узнать свой айди - @get_any_telegram_id_bot
 
 DIR = "" # Путь к файлу, если нужно
 
 #USER: # https://my.telegram.org/auth
-API_ID = 6623613
-API_HASH = "3504ada74bdd34a2e5bf510e1bfd6460"
+API_ID = 11156935
+API_HASH = "b52f362a105e149a5b6c29bd31c64f7f"
